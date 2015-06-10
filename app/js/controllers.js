b481fe7f55704ec522895b4f0fbab21b5881db61
Daniel Kalman <daniel@aniways.com>
2015-06-10 11:21:58 +0300

    Change telegram string to Aniways Messenger

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -235,7 +235,7 @@ angular.module('myApp.controllers', ['myApp.i18n'])
         })['finally'](function () {
           if ($rootScope.idle.isIDLE || tsNow() - authKeyStarted > 60000) {
             NotificationsManager.notify({
-              title: 'Telegram',
+              title: 'Aniways Messenger',
               message: 'Your authorization key was successfully generated! Open the app to log in.',
               tag: 'auth_key'
             });
